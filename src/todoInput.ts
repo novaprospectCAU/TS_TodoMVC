a084abdb7c52a4e9c418fd3108cfa38a67763823
novaprospectCAU <novaprospect110a@gmail.com>
2024-03-08 19:23:34 +0900

    js파일들을 삭제했습니다

--- src/todoInput.ts
+++ src/todoInput.ts
@@ -31,7 +31,7 @@ function pushItem(id: number, text: string) {
  * 새로운 입력값을 받는 함수
  */
 export function newInput() {
-  const inputSpace = <HTMLInputElement>document.querySelector(".todo-input");
+  const inputSpace = document.querySelector(".todo-input") as HTMLInputElement;
   inputSpace.addEventListener("keyup", (e) => {
     if (e.key === "Enter") {
       const string = inputCheck(inputSpace.value);
